11fa24e5509e59b2b4f0208e21760288295b37ed
Thorch3n <relax.041998@gmail.com>
2024-07-26 11:26:15 +0500

    Создание представлений

--- engineering_teams/teams/views.py
+++ engineering_teams/teams/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Team
+from .serializers import TeamSerializer
 
-# Create your views here.
+
+class TeamViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Team.objects.all()
+        serializer = TeamSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = TeamSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        try:
+            team = Team.objects.get(pk=pk)
+        except Team.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = TeamSerializer(team, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        try:
+            team = Team.objects.get(pk=pk)
+        except Team.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
